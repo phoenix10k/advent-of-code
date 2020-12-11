5e1ee3e049d5b5c9fa4845356ef4abd34cdaa8c2
Will Plano <will.plano@gmail.com>
2020-12-11 13:45:43 +0000

    Part 2

--- aoc-2020/python/day-11/main.py
+++ aoc-2020/python/day-11/main.py
@@ -5,8 +5,8 @@ def print_state():
     count = 0
     for line in state:
         count+= line.count('#')
-        print(line)
-    print('count:', count)
+        #print(line)
+    print('count:', count, '\n')
 
 
 def iter_state():
@@ -19,15 +19,24 @@ def iter_state():
 
             for dir_x, dir_y in [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1),
                                  (-1, -1), (-1, 0), (-1, 1)]:
-                test_y = y + dir_y
-                test_x = x + dir_x
-                if 0 <= test_y < len(state) and 0 <= test_x < len(state[y]):
-                    neighbours += state[test_y][test_x] == '#'
+                test_y = y
+                test_x = x
+                while True:
+                    test_y += dir_y
+                    test_x += dir_x
+                    if 0 <= test_y < len(state) and 0 <= test_x < len(state[y]):
+                        if state[test_y][test_x] == '#':
+                            neighbours += 1
+                            break
+                        if state[test_y][test_x] == 'L':
+                            break
+                    else:
+                        break
 
             if state[y][x] == 'L' and neighbours == 0:
                 new_state[y] += '#'
                 changes = True
-            elif state[y][x] == '#' and neighbours >= 4:
+            elif state[y][x] == '#' and neighbours >= 5:
                 new_state[y] += 'L'
                 changes = True
             else:

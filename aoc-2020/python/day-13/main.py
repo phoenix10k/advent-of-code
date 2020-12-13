1ac42d52f390b577e0c1fbf694da03a0344283e6
Will Plano <will.plano@gmail.com>
2020-12-13 21:54:49 +0000

    part 2 some headscratching to come up with this :smile:

--- aoc-2020/python/day-13/main.py
+++ aoc-2020/python/day-13/main.py
@@ -1,20 +1,28 @@
 import sys
+import math
 
 with open('input.txt') as file:
     start = int(next(file).strip())
-    buses = [int(x) for x in next(file).strip().split(',') if x != 'x']
+    buses = [(int(x), idx) for idx, x in enumerate(next(file).strip().split(',')) if x != 'x']
 
 print('start', start)
 print('buses', buses)
 
-minb = None
-mintime = None
-for b in buses:
-    time = b * ((start // b) + 1)
-    print('b:', b, 'time:', time)
-    if not mintime or time < mintime:
-        minb = b
-        mintime = time
+# minb = None
+# mintime = None
+# for b in buses:
+#     time = b * ((start // b) + 1)
+#     print('b:', b, 'time:', time)
+#     if not mintime or time < mintime:
+#         minb = b
+#         mintime = time
 
-print('ans:', minb * (mintime - start))
+# print('ans:', minb * (mintime - start))
 
+cnt = 1
+skip = 1
+for b, idx in buses:
+    while (cnt + idx) % b:
+        cnt += skip
+    print('b:', b, 'cnt:', cnt)
+    skip = (b * skip) // math.gcd(b, skip)

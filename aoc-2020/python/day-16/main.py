613a0a23515b77ec4b7359c43d95098b7d214d51
Will Plano <will.plano@gmail.com>
2020-12-16 14:26:12 +0000

    part 2

--- aoc-2020/python/day-16/main.py
+++ aoc-2020/python/day-16/main.py
@@ -1,3 +1,6 @@
+import functools
+import operator
+
 rules = {}
 other_tickets = []
 with open('input.txt') as file:
@@ -33,3 +36,31 @@ for t in other_tickets:
             errors += i
 
 print('errors:', errors)
+
+candidates = {k: set(range(len(rules))) for k in rules}
+for t in other_tickets:
+    if any(not any(r[0] <= i <= r[1] for rr in rules.values() for r in rr)
+           for i in t):
+        continue
+
+    for idx, i in enumerate(t):
+        for k, rr in rules.items():
+            if not any(r[0] <= i <= r[1] for r in rr):
+                candidates[k].remove(idx)
+
+print('candidates:', candidates)
+
+mapping = {}
+while len(mapping) < len(rules):
+    for k, s in candidates.items():
+        if len(s) == 1:
+            mapping[k] = s.pop()
+        s.difference_update(mapping.values())
+
+print('mapping:', mapping)
+print(
+    'answer:',
+    functools.reduce(
+        operator.mul,
+        [my_ticket[mapping[k]]
+         for k in rules if k.startswith('departure')], 1))

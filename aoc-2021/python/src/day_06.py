424b2c078c65f3f05a48efb9efd15e995f81b065
Will Plano <will.plano@gmail.com>
2021-12-08 14:08:17 +0000

    better solution for day 6

--- aoc-2021/python/src/day_06.py
+++ aoc-2021/python/src/day_06.py
@@ -1,24 +1,25 @@
 import os
-
-import numpy as np
-import numpy.typing as npt
+from typing import Dict
 
 
-def run_sim(fish: npt.NDArray[np.int_]) -> npt.NDArray[np.int_]:
-    next_fish: npt.NDArray[np.int_] = fish - 1
-    births: npt.NDArray[np.bool_] = next_fish < 0
-    next_fish = np.where(births, 6, next_fish)
-    num_births = np.count_nonzero(births)
-    new_fish = np.full(num_births, 8)
-    return np.concatenate((next_fish, new_fish))  # type: ignore
+def run_sim(fish: Dict[int, int]) -> Dict[int, int]:
+    new_fish = {}
+    for i in range(9):
+        new_fish[i] = fish.get(i + 1, 0)
+    new_fish[6] += fish.get(0, 0)
+    new_fish[8] = fish.get(0, 0)
+    return new_fish
 
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
 
     with open("../data/day_06.in") as input_file:
-        fish = np.array([int(f) for f in next(input_file).split(",")], dtype=int)
+        fish_list = [int(f) for f in next(input_file).split(",")]
+    fish = {k: 0 for k in range(9)}
+    for f in fish_list:
+        fish[f] += 1
 
     for i in range(256):
         fish = run_sim(fish)
-        print(f"day {i}: {len(fish)}")
+        print(f"day {i}: ({sum(fish.values())}) {fish}")
--- aoc-2021/python/test/test_day_06.py
+++ aoc-2021/python/test/test_day_06.py
@@ -7,16 +7,19 @@ from day_06 import run_sim
 def test_part_1() -> None:
     os.chdir(os.path.dirname(__file__))
     with open("data/test_day_06.in") as input_file:
-        fish = np.array([int(f) for f in next(input_file).split(",")])
+        fish_list = [int(f) for f in next(input_file).split(",")]
+    fish = {k: 0 for k in range(9)}
+    for f in fish_list:
+        fish[f] += 1
 
     for i in range(80):
         fish = run_sim(fish)
         print(f"day {i}: {fish}")
 
-    assert len(fish) == 5934
+    assert sum(fish.values()) == 5934
 
     for i in range(256 - 80):
         fish = run_sim(fish)
         print(f"day {i}: {fish}")
 
-    assert len(fish) == 26984457539
+    assert sum(fish.values()) == 26984457539

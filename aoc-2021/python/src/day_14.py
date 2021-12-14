225025c2a947b91966c1a631a8a690027bc4b526
Will Plano <will.plano@gmail.com>
2021-12-14 14:13:57 +0000

    Day 14 Part 2
    Sneaky

--- aoc-2021/python/src/day_14.py
+++ aoc-2021/python/src/day_14.py
@@ -1,24 +1,38 @@
 import os
 from collections import defaultdict
-from itertools import pairwise
+from itertools import count, pairwise
 from typing import TextIO
 
 
-def parse_input(input_file: TextIO) -> tuple[str, dict[str, str]]:
+def parse_input(input_file: TextIO) -> tuple[dict[str, int], dict[str, str]]:
     polymer = next(input_file).strip()
     next(input_file)  # skip blank line
     rules: dict[str, str] = {}
     for line in input_file:
         k, v = line.strip().split(" -> ")
         rules[k] = v
-    return polymer, rules
-
-
-def pair_insertion(polymer: str, rules: dict[str, str]) -> str:
-    output = polymer[0]
+    polymer_counts: defaultdict[str, int] = defaultdict(int)
+    for element in polymer:
+        polymer_counts[element] += 1
     for pair in pairwise(polymer):
-        output += rules["".join(pair)] + pair[1]
-    return output
+        polymer_counts["".join(pair)] += 1
+
+    return polymer_counts, rules
+
+
+def pair_insertion(polymer: dict[str, int], rules: dict[str, str]) -> dict[str, int]:
+    polymer_counts: defaultdict[str, int] = defaultdict(int)
+    polymer_counts.update(count_elements(polymer))
+    for k, v in polymer.items():
+        if len(k) == 2:
+            polymer_counts[k[0] + rules[k]] += v
+            polymer_counts[rules[k] + k[1]] += v
+            polymer_counts[rules[k]] += v
+    return polymer_counts
+
+
+def count_elements(polymer: dict[str, int]) -> dict[str, int]:
+    return {k: v for k, v in polymer.items() if len(k) == 1}
 
 
 if __name__ == "__main__":
@@ -30,8 +44,12 @@ if __name__ == "__main__":
     for _ in range(10):
         polymer = pair_insertion(polymer, rules)
 
-    counts: defaultdict[str, int] = defaultdict(int)
-    for c in polymer:
-        counts[c] += 1
+    counts = count_elements(polymer)
 
     print("part 1:", max(counts.values()) - min(counts.values()))
+
+    for _ in range(30):
+        polymer = pair_insertion(polymer, rules)
+
+    counts = count_elements(polymer)
+    print("part 2:", max(counts.values()) - min(counts.values()))
--- aoc-2021/python/test/test_day_14.py
+++ aoc-2021/python/test/test_day_14.py
@@ -1,7 +1,6 @@
 import os
-from collections import defaultdict
 
-from day_14 import pair_insertion, parse_input
+from day_14 import count_elements, pair_insertion, parse_input
 
 
 def test_part_1() -> None:
@@ -9,26 +8,26 @@ def test_part_1() -> None:
     with open("data/test_day_14.in") as input_file:
         polymer, rules = parse_input(input_file)
 
-    assert polymer == "NNCB"
-    polymer = pair_insertion(polymer, rules)
-    assert polymer == "NCNBCHB"
-    polymer = pair_insertion(polymer, rules)
-    assert polymer == "NBCCNBBBCBHCB"
-    polymer = pair_insertion(polymer, rules)
-    assert polymer == "NBBBCNCCNBBNBNBBCHBHHBCHB"
-    polymer = pair_insertion(polymer, rules)
-    assert polymer == "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
-    polymer = pair_insertion(polymer, rules)
-    assert len(polymer) == 97
-    for _ in range(5):
+    for _ in range(10):
         polymer = pair_insertion(polymer, rules)
-    assert len(polymer) == 3073
 
-    counts: defaultdict[str, int] = defaultdict(int)
-    for c in polymer:
-        counts[c] += 1
+    counts = count_elements(polymer)
 
     assert counts["B"] == 1749
     assert counts["C"] == 298
     assert counts["H"] == 161
     assert counts["N"] == 865
+
+
+def test_part_2() -> None:
+    os.chdir(os.path.dirname(__file__))
+    with open("data/test_day_14.in") as input_file:
+        polymer, rules = parse_input(input_file)
+
+    for _ in range(40):
+        polymer = pair_insertion(polymer, rules)
+
+    counts = count_elements(polymer)
+
+    assert counts["B"] == 2192039569602
+    assert counts["H"] == 3849876073

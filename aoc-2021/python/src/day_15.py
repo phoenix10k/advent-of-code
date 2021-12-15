86f005a862cbaad47f454ea69365b5e45a9c8bfe
Will Plano <will.plano@gmail.com>
2021-12-15 14:18:19 +0000

    Day 15 part 2

--- aoc-2021/python/src/day_15.py
+++ aoc-2021/python/src/day_15.py
@@ -5,14 +5,16 @@ from typing import TextIO
 import networkx as nx
 
 
-def parse_input(input_file: TextIO) -> nx.Graph:
+def parse_input(input_file: TextIO) -> list[list[int]]:
+    return [[int(r) for r in line.strip()] for line in input_file]
 
+
+def build_graph(input_data: list[list[int]]) -> nx.Graph:
+    size = len(input_data)
     graph = nx.DiGraph()
-    for i, line in enumerate(input_file):
-        line = line.strip()
-        size = len(line)
+    for i, line in enumerate(input_data):
         for j, risk in enumerate(line):
-            graph.add_node((i, j), risk=int(risk))
+            graph.add_node((i, j), risk=risk)
 
     for i1, i2 in pairwise(range(size)):
         for j in range(size):
@@ -33,10 +35,27 @@ def least_risk_path(graph: nx.Graph, size: int) -> int:
     )
 
 
+def inc_risk(risk: int, inc: int) -> int:
+    return 1 + ((risk - 1 + inc) % 9)
+
+
+def grow_map(input_data: list[list[int]]) -> list[list[int]]:
+    size = len(input_data)
+    return [
+        [inc_risk(r, i // size + j // size) for j, r in enumerate(row * 5)]
+        for i, row in enumerate(input_data * 5)
+    ]
+
+
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
 
     with open("../data/day_15.in") as input_file:
-        graph, size = parse_input(input_file)
+        input_data = parse_input(input_file)
 
+    graph, size = build_graph(input_data)
     print("part 1:", least_risk_path(graph, size))
+
+    bigger_data = grow_map(input_data)
+    graph, size = build_graph(bigger_data)
+    print("part 2:", least_risk_path(graph, size))
--- aoc-2021/python/test/test_day_15.py
+++ aoc-2021/python/test/test_day_15.py
@@ -1,12 +1,23 @@
 import os
 
 import networkx as nx
-from day_15 import least_risk_path, parse_input
+from day_15 import build_graph, grow_map, least_risk_path, parse_input
 
 
 def test_part_1() -> None:
     os.chdir(os.path.dirname(__file__))
     with open("data/test_day_15.in") as input_file:
-        graph, size = parse_input(input_file)
+        graph, size = build_graph(parse_input(input_file))
 
     assert least_risk_path(graph, size) == 40
+
+
+def test_part_2() -> None:
+    os.chdir(os.path.dirname(__file__))
+    with open("data/test_day_15.in") as input_file:
+        input_data = parse_input(input_file)
+
+    bigger_data = grow_map(input_data)
+    graph, size = build_graph(bigger_data)
+
+    assert least_risk_path(graph, size) == 315
